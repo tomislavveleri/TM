2966ce56605c627be62dfcede4cb1e8418ac0558
tomislavveleri <tomislavmatejcic0@gmai.com>
2024-12-05 13:39:38 +0100

    kraj kolokvija grupa 6

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TM
 https://github.com/tomislavveleri/TM
+
+SELECT zaposlenik, SUM(broj_sati) AS ukupni_sati
+FROM evidencija
+GROUP BY zaposlenik;
+
---
+++ tm/src/pages/IndexPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card">
+      <q-card-section>
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+
+    <q-card
+      class="my-card text-white"
+      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+    >
+      <q-card-section>
+        <div class="text-h6">Grupa 6 kolokvij</div>
+        <div class="text-subtitle2">by Tomislav Matejcic</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+
+  </div>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
